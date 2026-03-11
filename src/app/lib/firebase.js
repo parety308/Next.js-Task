cf54969e523a9519c76466e176106b378cb3c461
parvez <parvezyesrat17032024@gmail.com>
2026-03-11 12:23:36 +0600

    add netlify config

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBWbaYrWPBeus2ES3bESSyqY41LIEPDt08",
-    authDomain: "coursehub-5968b.firebaseapp.com",
-    projectId: "coursehub-5968b",
-    storageBucket: "coursehub-5968b.firebasestorage.app",
-    messagingSenderId: "479955736804",
-    appId: "1:479955736804:web:ce5026984b4b90960674f1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
